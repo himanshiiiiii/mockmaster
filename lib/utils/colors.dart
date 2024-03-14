771a968bf16addf6b0b063ae542e3ced0efa3993
ayushgit2003 <ayush123tripathi.mt@gmail.com>
2024-03-14 15:25:28 +0530

    text color change

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 const buttonColor = Color(0xffC3B1E1);
-const textColor=Color(0xffBF40BF);
+// const textColor=Color(0xffBF40BF);
+const textColor=Color(0xff000000);
